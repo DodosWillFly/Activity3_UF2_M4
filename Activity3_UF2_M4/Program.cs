6c98fd541db08190eb518eb1e6c4219823648020
Souhaila <souhailairzi@gmail.com>
2024-02-21 20:25:42 +0100

    el segon

--- Activity3_UF2_M4/Program.cs
+++ Activity3_UF2_M4/Program.cs
@@ -29,7 +29,9 @@
             Console.WriteLine(message);
             Console.ReadKey();
         }
-
+        /// <summary>
+        /// This function is to show the options of the program
+        /// </summary>
         public static void ShowOptions()
         {
             Console.WriteLine("1. Facebook");
